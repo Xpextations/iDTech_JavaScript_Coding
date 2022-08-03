8a8dac11457c740cfcbb0af801ceda34e4e18a2e
Xpextations <Jw.xpext@gmail.com>
2022-08-03 11:02:28 -0500

    added story time

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+let welcome = "Welcome to my world!"
+let name = "Gord"
+let title = "protector of all living things"
+let worldname = "purgatory"
+
+let age = 18000
+let year = 2200000
+let century = 22000
+
+let powers = ['sonic blast','regeneration','teleportation','life']
+let bestpower = true
+
+console.log(welcome + " I am " + name + " and I am " + title + ".")
+console.log("As the protector of " + worldname + " I have different powers to maintain life. ")
+console.log("This world is right under the Heavens, so this world has lasted longer than yours, which is year " + year + " and the " + century + " th century.")
+console.log("I am " + age + "years old.")
+console.log("Me: What is the power you use to help those in need?")
+console.log("Gord:" + bestpower + ", the power that helps the most is " + powers[3] + ".")
+console.log("Now begin your new journey in " + worldname + ".")
